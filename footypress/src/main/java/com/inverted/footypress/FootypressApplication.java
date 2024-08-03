f8fb0327a6dea3024f971e10583507a3cf74986a
phenyopilatso <pilatsophenyo@gmail.com>
2024-08-03 19:30:20 +0200

    create spring scaffolding

---
+++ footypress/src/main/java/com/inverted/footypress/FootypressApplication.java
@@ -0,0 +1,13 @@
+package com.inverted.footypress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootypressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootypressApplication.class, args);
+	}
+
+}
---
+++ footypress/src/test/java/com/inverted/footypress/FootypressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inverted.footypress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootypressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
